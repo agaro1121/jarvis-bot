17bb379b4e0bdc60d7bdc5f541dd7a562bde86e5
Anthony Garo <agaro1121@gmail.com>
2017-04-16 18:12:18 -0400

    attempts to split out some functions

--- scripts/jarvis-func.js
+++ scripts/jarvis-func.js
@@ -1,22 +1,35 @@
 'use strict';
 
+let config = require('../conf/config.json');
+let gitConfig = config.github;
+
 module.exports = (robot) => {
+
   robot.respond(/show (\w*)/i, (res) => {
-    let username = res.match[1];
-    let userEmail = res.message.user.profile.email;
-    console.log(userEmail);
-    // if(userEmail == "") { //works !!!!!
-      res.http(`https://api.github.com/users/${username}`)
-        .get()((err, response, body) => {
-          console.log("activated http callback");
 
-          // res.send(response.statusMessage);
-          res.send("```\n" + JSON.stringify(JSON.parse(body), null, 2) + "\n```"); //works
+      let username = res.match[1]; //from slack message
+      let userEmailFromSlack = res.message.user.profile.email; //from slack payload
 
+      if(isUserEmailAuthorized(userEmailFromSlack)) {
+        getUserInfoAndRespondToSlack(res, username);
+      } else {
+        res.send("unauthorized user");
+      }
 
-        });
-    // } else {
-      res.send("unauthorized user");
-    // }
   });
-}
+};
+
+let isUserEmailAuthorized = (userEmailFromSlack) => {
+  return gitConfig.allowedUsers.some(user => user === userEmailFromSlack);
+};
+
+let formatBody = (body) => {
+  return "```" + JSON.stringify(JSON.parse(body), null, 2) + "```";
+};
+
+let getUserInfoAndRespondToSlack = (res, username) => {
+ res.http(`${gitConfig.url}${username}`) //TODO: how to return this ???
+    .get()((err, response, body) => {
+      res.send(formatBody(body));
+    });
+};
